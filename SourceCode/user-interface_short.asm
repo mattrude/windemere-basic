d548789ea6aabffcd8c99444e37f1515bdb9f087
Matt Rude <matt@mattrude.com>
2024-02-01 11:35:27 -0600

    Correct top header spacing

--- SourceCode/user-interface_short.asm
+++ SourceCode/user-interface_short.asm
@@ -100,6 +100,8 @@
 ; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 ; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 ; DEALINGS IN THE SOFTWARE.
+;
+; -----------------------------------------------------------------------------
 
    ;.setcpu "65C02"             ; Declare the CPU type
     .org $FE00                  ; Start at memory location 8000 hex
